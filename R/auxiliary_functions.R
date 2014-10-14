a06a78db82caec8f10b9d7316a7de1ff71f6f5bb
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2014-10-14 17:14:36 +0200

    Removed source code from examples.

--- R/auxiliary_functions.R
+++ R/auxiliary_functions.R
@@ -7,25 +7,25 @@
 
 
 #' Auxiliary functions for package RNAstr
-#' 
+#'
 #' Auxiliary functions for package RNAstr
-#' 
-#' 
+#'
+#'
 
 
 
 
 #' Auxiliary functions for package RNAstr
-#' 
+#'
 #' Auxiliary functions for package RNAstr
-#' 
+#'
 #' Make smoothing matrix
-#' 
+#'
 #' Auxiliary function speeding up calculations in functions operating on
 #' sliding windows.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param input_vector input_vector
 #' @param window_size window_size
 #' @return Matrix with (length(input_vector)+window_size-1) columns and
@@ -36,24 +36,12 @@
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' construct_smoothing_matrix(1:10, 3)
-#' 
-#' ## The function is currently defined as
-#' function (input_vector, window_size)
-#' {
-#'     vector_length <- length(input_vector)
-#'     my_mat <- matrix(nrow = window_size, ncol = (vector_length +
-#'         window_size - 1))
-#'     for (i in 1:(window_size)) {
-#'         my_mat[i, i:(vector_length + i - 1)] <- input_vector
-#'     }
-#'     return(my_mat)
-#'   }
-#' 
+#'
 #' @export construct_smoothing_matrix
 construct_smoothing_matrix <- function(input_vector, window_size){
-    
+
     vector_length <- length(input_vector)
     #Create empty matrix.
     my_mat <- matrix(nrow=window_size, ncol=(vector_length+window_size-1))
@@ -62,7 +50,7 @@ construct_smoothing_matrix <- function(input_vector, window_size){
     {
         my_mat[i,i:(vector_length+i-1)] <- input_vector
     }
-    
+
     return(my_mat)
 }
 
@@ -78,11 +66,11 @@ construct_smoothing_matrix <- function(input_vector, window_size){
 
 
 #' Calculate moving average
-#' 
+#'
 #' Moving average calculation. NA values ignored.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param input_vector input_vector
 #' @param window_size window_size
 #' @return numeric vector. Each position represents the mean of values in
@@ -93,23 +81,14 @@ construct_smoothing_matrix <- function(input_vector, window_size){
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' moving_average(1:10, 5)
-#' 
-#' ## The function is currently defined as
-#' function (input_vector, window_size)
-#' {
-#'     window_side <- window_size/2 - 0.5
-#'     return(colMeans(construct_smoothing_matrix(input_vector,
-#'         window_size), na.rm = TRUE)[(window_side + 1):(length(input_vector) +
-#'         window_side)])
-#'   }
-#' 
+#'
 #' @export moving_average
 moving_average <- function(input_vector, window_size)
 {
     window_side <- window_size/2-0.5
-    return(colMeans(construct_smoothing_matrix(input_vector, window_size), 
+    return(colMeans(construct_smoothing_matrix(input_vector, window_size),
         na.rm=TRUE)[(window_side+1):(length(input_vector)+window_side)])
 }
 
@@ -125,13 +104,13 @@ moving_average <- function(input_vector, window_size)
 
 
 #' Winsor normalization with fitting to <0,1> range.
-#' 
+#'
 #' Function performs Winsor normalization of a supplied vector, sets bottom
 #' (winsorized) value to 0, top 1 and linearly transforms all the values to the
 #' <0,1> range.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param input_vector Vector with values to be Winsorized
 #' @param winsor_level Winsorization level. Bottom outliers will be set to
 #' (1-winsor_level)/2 quantile and top outliers to (1+winsor_level)/2 quantile.
@@ -147,38 +126,17 @@ moving_average <- function(input_vector, window_size)
 #' 413--426.
 #' @keywords winsorising ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' data_set <- runif(1:100)*100
 #' plot(winsor(data_set, winsor_level=0.8) ~ data_set)
-#' 
-#' 
-#' ## The function is currently defined as
-#' function (input_vector, winsor_level = 0.9, only_top = FALSE)
-#' {
-#'     bounds <- quantile(input_vector, c((1 - winsor_level)/2,
-#'         1 - (1 - winsor_level)/2), names = FALSE, na.rm = TRUE)
-#'     if (only_top) {
-#'         bounds[1] <- 0
-#'     }
-#'     input_vector[input_vector < bounds[1]] <- bounds[1]
-#'     input_vector[input_vector > bounds[2]] <- bounds[2]
-#'     if ((bounds[2] - bounds[1]) > 0) {
-#'         input_vector <- (input_vector - bounds[1])/(bounds[2] -
-#'             bounds[1])
-#'     }
-#'     else {
-#'         input_vector[1:length(input_vector)] <- NA
-#'     }
-#'     return(input_vector)
-#'   }
-#' 
+#'
 #' @export winsor
 winsor <- function(input_vector, winsor_level=0.9, only_top= FALSE)
-{ 
+{
     #!!! Function changes integers to numerics!
-    bounds <- quantile(input_vector, c((1-winsor_level)/2, 
+    bounds <- quantile(input_vector, c((1-winsor_level)/2,
         1-(1-winsor_level)/2), names= FALSE, na.rm=TRUE)
-    
+
     if(only_top){bounds[1] <- 0}
     input_vector[input_vector < bounds[1]] <- bounds[1]
     input_vector[input_vector > bounds[2]] <- bounds[2]
@@ -203,15 +161,15 @@ winsor <- function(input_vector, winsor_level=0.9, only_top= FALSE)
 
 
 #' Smooth Winsor Normalization
-#' 
+#'
 #' Function performs Winsor normalization (see winsor() function) of each
 #' window of specified window_size, sliding in a given vector by 1 position,
 #' and reports a list of (1) mean Winsorized values for each vector position
 #' (mean of Winsorized value for a given position as calculated within each
 #' overlapping window) and (2) standard deviation of those Winsorized values.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param input_vector Vector with values to be smooth-Winsorized
 #' @param window_size Size of a sliding window.
 #' @param winsor_level Winsorization level. Bottom outliers will be set to
@@ -221,7 +179,7 @@ winsor <- function(input_vector, winsor_level=0.9, only_top= FALSE)
 #' @return \item{comp1}{Vector with mean Winsorized values for each
 #' input_vector position} \item{comp2 }{Vector with standard deviation of
 #' Winsorized values for each input_vector position}
-#' 
+#'
 #' %% ...
 #' @note %% ~~further notes~~
 #' @author Lukasz Jan Kielpinski
@@ -230,45 +188,19 @@ winsor <- function(input_vector, winsor_level=0.9, only_top= FALSE)
 #' In review as for 2nd Oct 2014.
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' data_set <- runif(1:100)*100
 #' plot(swinsor_vector(data_set, window_size=71, winsor_level=0.8)[[1]] ~ data_set)
-#' 
-#' 
-#' ## The function is currently defined as
-#' function (input_vector, window_size, winsor_level = 0.9, only_top = FALSE)
-#' {
-#'     align_smoothing_matrix <- function(input_matrix) {
-#'         vector_length <- ncol(input_matrix) - nrow(input_matrix) +
-#'             1
-#'         my_mat <- matrix(nrow = nrow(input_matrix), ncol = vector_length)
-#'         for (i in 1:nrow(input_matrix)) {
-#'             my_mat[i, ] <- input_matrix[i, i:(vector_length +
-#'                 i - 1)]
-#'         }
-#'         return(my_mat)
-#'     }
-#'     my_matrix <- construct_smoothing_matrix(input_vector, window_size)
-#'     winsorized_matrix <- apply(my_matrix, 2, winsor, winsor_level = winsor_level,
-#'         only_top = only_top)
-#'     winsorized_matrix[, c(1:(window_size - 1), (length(input_vector) +
-#'         1):ncol(winsorized_matrix))] <- NA
-#'     aligned_winsorized_matrix <- align_smoothing_matrix(winsorized_matrix)
-#'     means_vector <- colMeans(aligned_winsorized_matrix, na.rm = TRUE)
-#'     sds_vector <- apply(aligned_winsorized_matrix, 2, FUN = sd,
-#'         na.rm = TRUE)
-#'     return(list(means_vector, sds_vector))
-#'   }
-#' 
+#'
 #' @export swinsor_vector
-swinsor_vector <- function(input_vector, window_size, winsor_level=0.9, 
+swinsor_vector <- function(input_vector, window_size, winsor_level=0.9,
     only_top= FALSE)
 {
-    #Define function for aligning matrix generated by 
+    #Define function for aligning matrix generated by
     #construct_smoothing_matrix
     align_smoothing_matrix <- function(input_matrix)
     {
-        #Length of the vector used to construct the matrix in 
+        #Length of the vector used to construct the matrix in
         #construct_smoothing_matrix function.
         vector_length <- ncol(input_matrix)-nrow(input_matrix)+1
         #Create empty matrix.
@@ -286,7 +218,7 @@ swinsor_vector <- function(input_vector, window_size, winsor_level=0.9,
     winsorized_matrix <- apply(my_matrix, 2, winsor, winsor_level=winsor_level,
         only_top=only_top)
     #Control the border behaviour
-    winsorized_matrix[,c(1:(window_size-1), 
+    winsorized_matrix[,c(1:(window_size-1),
         (length(input_vector)+1):ncol(winsorized_matrix))] <- NA
     aligned_winsorized_matrix <- align_smoothing_matrix(winsorized_matrix)
     means_vector <- colMeans(aligned_winsorized_matrix, na.rm=TRUE)
@@ -308,11 +240,11 @@ swinsor_vector <- function(input_vector, window_size, winsor_level=0.9,
 #' Function to make data frame out of GRanges output of normalizing functions
 #' (dtcr(), slograt(), swinsor(), compdata()) for all or a set of chosen
 #' transcripts in the file.
-#' 
+#'
 #' Simple convenience function.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param norm_GR GRanges object made by other normalization function (dtcr(),
 #' slograt(), swinsor(), compdata()) from which data is to be extracted
 #' @param RNAid Transcript identifiers of transcripts that are to be extracted
@@ -326,31 +258,12 @@ swinsor_vector <- function(input_vector, window_size, winsor_level=0.9,
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' dummy_euc_GR_treated <- GRanges(seqnames="DummyRNA", IRanges(start=round(runif(100)*100), width=round(runif(100)*100+1)), strand="+", EUC=round(runif(100)*100))
 #' dummy_comp_GR_treated <- comp(dummy_euc_GR_treated)
 #' dummy_swinsor <- swinsor(dummy_comp_GR_treated)
 #' GR2norm_df(dummy_swinsor)
-#' 
-#' 
-#' ## The function is currently defined as
-#' function (norm_GR, RNAid="all", norm_methods="all")
-#' {
-#'     if(norm_methods=="all" || missing(norm_methods))
-#'     {
-#'         norm_methods <- names(mcols(norm_GR))
-#'     }
-#' 
-#'     if(RNAid=="all" || missing(RNAid))
-#'     {
-#'         RNAid <- levels(seqnames(norm_GR))
-#'     }
-#' 
-#'     norm_GR <- norm_GR[seqnames(norm_GR) %in% RNAid, norm_methods]
-#'     data.frame(RNAid = as.character(seqnames(norm_GR)), Pos = as.integer(start(norm_GR)),
-#'         mcols(norm_GR))
-#'   }
-#' 
+#'
 #' @import GenomicRanges
 #' @export GR2norm_df
 GR2norm_df <- function(norm_GR, RNAid="all", norm_methods="all")
@@ -362,10 +275,10 @@ GR2norm_df <- function(norm_GR, RNAid="all", norm_methods="all")
     }
     if(RNAid=="all" || missing(RNAid)){RNAid <- levels(seqnames(norm_GR))}
 
-    # Parsing normalized file based on the selected RNAid(s) and 
+    # Parsing normalized file based on the selected RNAid(s) and
     # normalization method(s).
     norm_GR <- norm_GR[seqnames(norm_GR) %in% RNAid, norm_methods]
-    data.frame(RNAid=as.character(seqnames(norm_GR)), 
+    data.frame(RNAid=as.character(seqnames(norm_GR)),
         Pos=as.integer(start(norm_GR)), mcols(norm_GR))
 }
 
@@ -382,11 +295,11 @@ GR2norm_df <- function(norm_GR, RNAid="all", norm_methods="all")
 
 
 #' Function to convert norm_df data frame (product of GR2norm_df()) to GRanges
-#' 
+#'
 #' Function to convert norm_df data frame (product of GR2norm_df()) to GRanges.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param norm_df norm_df data frame needs to have columns: RNAid (equivalent
 #' to seqnames in GRanges) and Pos (equivalent to start in GRanges) and
 #' metadata
@@ -394,26 +307,18 @@ GR2norm_df <- function(norm_GR, RNAid="all", norm_methods="all")
 #' (dtcr(), slograt(), swinsor(), compdata())
 #' @note %% ~~further notes~~
 #' @author Lukasz Jan Kielpinski
-#' @seealso \code{\link{dtcr}}, \code{\link{slograt}}, \code{\link{swinsor}}, 
+#' @seealso \code{\link{dtcr}}, \code{\link{slograt}}, \code{\link{swinsor}},
 #' \code{\link{compdata}}, \code{\link{GR2norm_df}}, \code{\link{norm2bedgraph}}
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' dummy_norm_df <- data.frame(RNAid="dummyRNA", Pos=1:100, my_data1=runif(1:100))
 #' norm_df2GR(dummy_norm_df)
-#' 
-#' ## The function is currently defined as
-#' function (norm_df)
-#' {
-#'     GRanges(seqnames = norm_df$RNAid, IRanges(start = norm_df$Pos,
-#'         width = 1), strand = "+", norm_df[names(norm_df) !=
-#'         "RNAid" & names(norm_df) != "Pos"])
-#'   }
-#' 
+#'
 #' @export norm_df2GR
 norm_df2GR <- function(norm_df)
 {
-    GRanges(seqnames=norm_df$RNAid, IRanges(start=norm_df$Pos, width=1), 
+    GRanges(seqnames=norm_df$RNAid, IRanges(start=norm_df$Pos, width=1),
         strand="+", norm_df[names(norm_df)!="RNAid" & names(norm_df)!="Pos"])
 }
--- R/k2n_calc.R
+++ R/k2n_calc.R
@@ -1,13 +1,13 @@
 #' Function to calculate number of Estimated Unique Counts (EUC's)
 #' corresponding to given number of observed unique barcodes.
-#' 
+#'
 #' Function calculates EUC's for each number of observed barcodes accounting
 #' for differential ligation probability of different barcodes. Function
 #' k2n_calc() writes file with a vector in which an i-th element is an
 #' estimated unique count given observing i unique barcodes.
-#' 
+#'
 #' %% ~~ If necessary, more details than the description above ~~
-#' 
+#'
 #' @param merged_file path to merged_temp file containing 4 column: 1) RNAid,
 #' 2) Start, 3) End, 4) Barcode sequence (required)
 #' @param unique_barcode_file character with path to unique_barcode file
@@ -25,57 +25,11 @@
 #' Nucleic Acids Res.
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
+#'
 #' ##---- Should be DIRECTLY executable !! ----
 #' ##-- ==>  Define data, use random,
 #' ##-- or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (merged_file, unique_barcode_file, output_file)
-#' {
-#'     merged <- read.table(merged_file)
-#'     barcode_length <- max(nchar(as.character(merged[, 4])))
-#'     read_counts <- count(merged[1:3])
-#'     max_observed <- max(read.table(unique_barcode_file)[, 4])
-#'     barcodes_nt <- merged[do.call(paste, as.list(merged[, 1:3])) %in%
-#'         do.call(paste, as.list(read_counts[(read_counts[, 4]) <=
-#'             summary(read_counts[, 4])[5], 1:3])), 4]
-#'     nt_counts <- matrix(nrow = 4, ncol = barcode_length)
-#'     for (h in 1:ncol(nt_counts)) {
-#'         j <- 1
-#'         for (nt_local in c("A", "C", "G", "T")) {
-#'             nt_counts[j, h] <- sum(substr(as.character(barcodes_nt),
-#'                 h, h) == nt_local)
-#'             j <- j + 1
-#'         }
-#'     }
-#'     nt_freqs <- nt_counts/colSums(nt_counts)
-#'     nt_values <- list()
-#'     for (i in 1:ncol(nt_freqs)) {
-#'         nt_values[[i]] <- nt_freqs[, i]
-#'     }
-#'     all_posible_comb <- expand.grid(nt_values)
-#'     probs <- apply(all_posible_comb, 1, prod)
-#'     results <- c()
-#'     i <- 1
-#'     results[i] <- sum(1 - (1 - probs)^i)
-#'     while (results[i] <= max_observed) {
-#'         i <- i + 1
-#'         results[i] <- sum(1 - (1 - probs)^i)
-#'     }
-#'     k2n <- c()
-#'     for (i in 1:floor(max(results))) {
-#'         difference <- abs(results - i)
-#'         k2n[i] <- which(difference == min(difference))
-#'     }
-#'     if (!missing(output_file)) {
-#'         write(k2n, file = output_file)
-#'     }
-#'     else {
-#'         return(k2n)
-#'     }
-#'   }
-#' 
+#'
 #' @import plyr
 #' @export k2n_calc
 k2n_calc <- function(merged_file, unique_barcode_file, output_file){
